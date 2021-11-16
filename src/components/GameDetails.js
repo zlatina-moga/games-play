e16739477e6abea45122bd4694c5697e9ea56c9e
Zlatina Moga <zlatina.delcheva@gmail.com>
2021-11-16 18:27:15 +0200

    add details page

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,24 +1,31 @@
+import { useState, useEffect } from "react";
+import * as gameService from './services/gameService';
+
+
 export default function GameDetails({
     id
 }) {
+    const [game, setGame] = useState([]);
+    useEffect(() => {
+        gameService.getOne(id)
+            .then(result => {
+                setGame(result)
+            })
+    }, [])
+
     return (
         <section id="game-details">
         <h1>Game Details</h1>
         <div className="info-section">
 
             <div className="game-header">
-                <img className="game-img" src="images/MineCraft.png" />
-                <h1>Bright</h1>
-                <span className="levels">MaxLevel: 4</span>
-                <p className="type">Action, Crime, Fantasy</p>
+                <img className="game-img" src={game.imageUrl} />
+                <h1>{game.title}</h1>
+                <span className="levels">{game.maxLevel}</span>
+                <p className="type">{game.category}</p>
             </div>
 
-            <p className="text">
-                Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                creatures live side by side with humans. A human cop is forced
-                to work with an Orc to find a weapon everyone is prepared to kill for.
-            </p>
+            <p className="text">{game.summary}</p>
 
 
             <div className="details-comments">
--- src/components/services/gameService.js
+++ src/components/services/gameService.js
@@ -1,4 +1,11 @@
+const baseUrl = 'http://localhost:3030/data'
+
 export function getAll() {
-    return fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+    return fetch(`${baseUrl}/games?sortBy=_createdOn%20desc`)
+        .then(res => res.json())
+}
+
+export function getOne(id) {
+    return fetch(`${baseUrl}/games/${id}`)
         .then(res => res.json())
 }
